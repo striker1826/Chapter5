7cc6b85bc57b3d7e978063df3071e421bddf9e76
LAPTOP-PQPCKK5H\alstj <alstjq123579@daum.net>
2022-10-14 22:00:37 +0900

    김민섭: membersRepo 데이터 생성 오류 2022-10-14 22:00

--- controller/members.controllers.js
+++ controller/members.controllers.js
@@ -2,6 +2,7 @@ const MembersService = require("../service/members.service");
 
 class MembersController {
   MembersService = new MembersService();
+
   createMembers = async (req, res, next) => {
     const { userId, nickname, password, confirmPw } = req.body;
     await this.MembersService.createMembers(
