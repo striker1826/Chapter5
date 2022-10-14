52a45aba40eba76e909f3de07d9767630dc2f4d3
LAPTOP-PQPCKK5H\alstj <alstjq123579@daum.net>
2022-10-15 00:38:51 +0900

    김민섭: 회원가입 아이디, 닉네임, 패스워드 조건 작성했습니다 22-10-15 00:38

--- controller/members.controllers.js
+++ controller/members.controllers.js
@@ -4,14 +4,18 @@ class MembersController {
   MembersService = new MembersService();
 
   createMembers = async (req, res, next) => {
-    const { userId, nickname, password, confirmPw } = req.body;
-    await this.MembersService.createMembers(
-      userId,
-      nickname,
-      password,
-      confirmPw
-    );
-    res.status(200).json({ message: "회원가입이 완료되었습니다" });
+    try {
+      const { userId, nickname, password, confirmPw } = req.body;
+      const result = await this.MembersService.createMembers(
+        userId,
+        nickname,
+        password,
+        confirmPw
+      );
+      res.status(result.status).json({ message: result.message });
+    } catch (e) {
+      res.json(e);
+    }
   };
 }
 
--- models/index.js
+++ models/index.js
@@ -42,6 +42,5 @@ Object.keys(db).forEach((modelName) => {
 });
 
 db.sequelize = sequelize;
-db.Sequelize = Sequelize;
 
 module.exports = db;
