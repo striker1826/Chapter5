df71a1451a4777738f8141ec5b5919efb54648a6
chamchimayo <chamchi.maayo@gmail.com>
2022-10-16 05:27:01 +0900

    백지영: 좋아요 기능 구현중 22-10-16 05:26

---
+++ routes/likes.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+
+const authMiddleware = require("../middlewares/auth-middleware");
+
+const LikesController = require("../controller/likes.controller");
+const likesController = new LikesController();
+
+// 게시글 좋아요
+// router.post("/post/:postId", authMiddleware, likesController.likePost);
+// 
+
+module.exports = router;
